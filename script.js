5e811d641095c12ecb4ea6bc0d86693d5c046455
zola107 <camillecolleauxpro123@gmail.com>
2025-11-13 15:12:23 +0000

    feat: Implémentation du toggle dark mode

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log ("le script est charger")
+ console.log("Le script est chargé !");
+
+
+const titre = document.querySelector('#titre-principal')
+if (titre) {
+titre.addEventListener('click', () => {
+    console.log("J'ai cliqué sur le titre !");
+    titre.textContent = "Titre cliqué !";
+  });
+}
+
+
+const themeButton = document.querySelector('#bouton1')
+if (themeButton) {  
+themeButton.addEventListener('click', () => {
+document.body.classList.toggle('dark-mode');
+  });
+}
+
+ 
